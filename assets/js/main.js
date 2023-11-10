cb2f3a44640c84df8e19d62adc91ccba460bda2c
itzshiv143 <shivsupertech@gmail.com>
2023-11-10 11:29:57 +0530

    desighn done firstly

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,4 +41,48 @@ window.addEventListener("scroll", scrollHeader);
 
 /*=============== CALCULATE JS ===============*/
 
+const calculateForm = document.getElementById('calculate-form'),
+      calculateCm = document.getElementById('calculate-cm'),
+      calculateMessage = document.getElementById('calculate-message'),
+      calculateKg = document.getElementById('calculate-kg');
+
+const calculateBmi = (e)=>{
+      e.preventDefault();
+      if(calculateCm.value === '' || calculateKg.value === ''){
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        calculateMessage.textContent = "Fill in the Height and Weight 👍"; 
+
+        setTimeout(() => {
+        calculateMessage.textContent = ""; 
+        }, 3000);
+      }
+      else{
+        const cm = calculateCm.value /100,
+              kg = calculateKg.value,
+              bmi = Math.round( kg / ( cm * cm ) );
+              
+        if(bmi< 18.5){
+          calculateMessage.classList.add('color-green')
+          calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny 😢`
+        }else if(bmi<25){
+          calculateMessage.classList.add('color-green')
+          calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy👌`
+        }
+        else{
+          calculateMessage.classList.add('color-green')
+          calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight 🥲`
+        }
+
+        calculateCm.value = ''
+        calculateKg.value = ''
+
+        setTimeout(()=>{
+          calculateMessage.textContent = ''
+        },4000)
+      }
+}
+
+calculateForm.addEventListener('submit', calculateBmi)
 /*=============== EMAIL JS ===============*/
